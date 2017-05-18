b806f14059b251581af22942e9fe5cd21a7ba9bc
Vikram Sugumaran <vsugumar@uci.edu>
2017-05-18 11:24:22 -0700

    Changed start state from (0,0) to (0.5, 0.5). Works much better. Also added some comments

--- sarsaattempt1.py
+++ sarsaattempt1.py
@@ -5,6 +5,7 @@ import sys
 import time
 import json
 import random
+import math
 
 import maze_gen2
 from collections import defaultdict
@@ -78,6 +79,7 @@ if agent_host.receivedArgument("help"):
     print agent_host.getUsage()
     exit(0)
 
+#loading mission file
 mission_file = "sarsaattempt1.xml"
 with open(mission_file, 'r') as f:
     print "Loading mission from %s" % mission_file
@@ -87,6 +89,9 @@ with open(mission_file, 'r') as f:
 my_mission = MalmoPython.MissionSpec(missionXML, True)
 my_mission_record = MalmoPython.MissionRecordSpec()
 
+#################
+#MAZE GENERATION#
+#################
 size = (DEFAULT_SIZE, DEFAULT_SIZE)
 if len(sys.argv) == 1:
     pass
@@ -113,21 +118,42 @@ for i in e:
 
 final_coords = ( (size[0]-1)*2, (size[1]-1)*2 )
 my_mission.drawBlock(final_coords[0], 3, final_coords[1], "lapis_block")
+###############
+###############
 
+#We want to reset the world everytime we run this program (so we don't
+#use a previously created maze) but for every episode within a run
+#of the program we don't want to reset the world (because it takes too
+#long). Malmo has no way to set the forceReset flag off as far as I'm
+#aware of, so we create a separate mission object that has the same
+#parameters (the maze in particular) as the current mission object and
+#leave the forceReset flag unset for the new one. We run my_mission
+#on the first episode to reset the world ("erasing" any mazes from previou
+#programs and run my_mission2 thereafter so we don't reset the world from 
+#then on
 my_mission2 = MalmoPython.MissionSpec(my_mission.getAsXML(False), True)
 my_mission_record2 = MalmoPython.MissionRecordSpec()
 my_mission.forceWorldReset()
 
+#Our agent shold be able to move in 4 directions
 actions = ["movenorth 1", "movesouth 1", "movewest 1", "moveeast 1"]
+
+#This takes a state and initializes it in the Q table (setting the values
+#to all be equal to 0 initially)
 def init_q_state(q_table, state, actions=actions):
     for a in actions:
         q_table[state][a] = 0
 
+#Create a Q table and initialize the start state
 q_table = defaultdict(dict)
-init_q_state(q_table, (0,0))
+init_q_state(q_table, (0.5,0.5))
 
+#Create a separate dict to keep track of the number of times we've
+#visited each state
 num_visited = defaultdict(int)
 
+#Given a list of action-value pairs, choose an action with an
+#epsilon-greedy policy
 def eps_greedy(actions, epsilon=0.1):
     r = random.random()
     if r < epsilon:
@@ -136,17 +162,19 @@ def eps_greedy(actions, epsilon=0.1):
     else:
         return max(actions, key=lambda x: x[1])[0]
 
-def update_q_table(q_table, s, a, r, snew, anew, alpha=0.1, gamma=0.5):
+#Updating a Q table w/ the sarsa update
+def update_q_table(q_table, s, a, r, snew, anew, alpha=0.3, gamma=0.9):
     q_table[s][a] += alpha*(r + gamma*q_table[snew][anew] - q_table[s][a])
 
 # Attempt to start a mission:
-num_repeats = 1000
+num_repeats = 10000 #(number of episodes)
 for i in range(num_repeats):
     print
     print("Repeat %d of %d" % (i+1, num_repeats))
 
     my_mission_record = MalmoPython.MissionRecordSpec()
 
+    #Attempt to start the mission
     max_retries = 1000
     for retry in range(max_retries):
         try:
@@ -175,9 +203,16 @@ for i in range(num_repeats):
     print
     print "Mission running ",
 
-    s = (0,0)
-    num_visited[s] += 1
+    #our start state is (0.5,0.5) by default 
+    s = (0.5,0.5)
+    num_visited[s] += 1 #We have visited this state once
+
+    #choose action from start state with epsilon greedy policy w/
+    #epsilon = 1/number of times this state has been visited
     a = eps_greedy(q_table[s].items(), 1/num_visited[s])
+
+    #print start state's q table at the beginning of each episode
+    #for debugging purposes
     print(q_table[s])
 
     # Loop until mission ends:
@@ -191,15 +226,19 @@ for i in range(num_repeats):
         for error in world_state.errors:
             print "Error:",error.text
 
+        #Some stuff I had to do to make it work smoothly
         obs_text = []
         while len(obs_text) > 0:
             obs_text = world_state.observations[-1].text
             world_state = agent_host.getWorldState()
 
         time.sleep(0.1)
+
+        #Take an action
         agent_host.sendCommand(a)
         num_steps += 1
 
+        #Will explain later (needed to make it work smoothly)
         obs_text = []
         breakloop = False
         count = 0
@@ -215,23 +254,35 @@ for i in range(num_repeats):
         if breakloop:
             break
 
+        #get the reward for this action
         r = 0
-
         for reward in world_state.rewards:
             r += reward.getValue()
 
+        
         obs = json.loads(obs_text)
-        snew = (obs["ZPos"], obs["XPos"])
+        snew = (obs["XPos"], obs["ZPos"])
+        #If we've never seen this state before,
+        #add it to the Q table and initialize it
         if snew not in q_table:
             init_q_state(q_table, snew)
         
-        dist = (abs(snew[0] - 0), abs(snew[1] - 0))
+        #calculate "Manhattan Distance" from start state
+        dist = (abs(snew[0] - 0.5), abs(snew[1] - 0.5))
         dist = dist[0] + dist[1]
-        r = r/(dist**4)
+
+        #Discount rewards by DISTANCE from the start state (unlike gamma,
+        #which discounts by time)
+        #So if the agent dies further away from the goal, it gets less
+        #negative reward
+        r = r/(dist**2)
         
-        num_visited[snew] += 1
-        anew = eps_greedy(q_table[snew].items(), 1/num_visited[snew])
-        update_q_table(q_table, s, a, r, snew, anew) #sarsa
+        #choose new action according to epsilon greedy policy with
+        #epsilon = 1/log(number of times we visited the state)
+        anew = eps_greedy(q_table[snew].items(), 1/(math.log(num_visited[snew]+2)))
+        update_q_table(q_table, s, a, r, snew, anew) #sarsa update to q table
+        
+        
         s = snew
         a = anew
         
